3092b11f69496afe975ac019c9a121e9b4544a29
Eric Torigian <eric@torigian.com>
2018-03-19 10:51:27 -0400

    Ready to build login system

--- tripNerd/Controller/ViewController.swift
+++ tripNerd/Controller/ViewController.swift
@@ -14,24 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
-    
-        let patientRecords = PFObject(className: "PatientRecords")
-        
-        patientRecords["name"] = "Eric"
-        patientRecords["address"] = "96 Stillmeadown lane"
-        patientRecords["balance"] = 100
-        
-        patientRecords.saveInBackground { (success, error) in
-            if error == nil {
-                print("record saved")
-            } else {
-                print(error!)
-            }
-        }
-        
-        
     }
-
+    
 }
 
